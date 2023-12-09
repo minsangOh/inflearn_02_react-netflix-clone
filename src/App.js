e30c023ef613980306ebb82b1de14230b94f3b0c
minsangOh <flos9537@gmail.com>
2023-12-09 21:54:11 +0900

    footer 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import requests from "./api/requests";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Row from "./components/Row";
 
@@ -20,6 +21,7 @@ function App() {
       <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      <Footer />
     </div>
   );
 }
