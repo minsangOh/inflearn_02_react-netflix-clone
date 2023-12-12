a67bcd2ea99a8a53dde688fdfa827a94b5bb043b
minsangOh <flos9537@gmail.com>
2023-12-12 17:04:30 +0900

    검색결과 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,38 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import"./Nav.css"
+import React, { useEffect, useState, useRef } from "react";
+import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
-
-  const [show, setShow] = useState(false)
+  const [srearchValue, setSrearchValue] = useState("");
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      console.log('window.screenY', window.screenY)
-      if (window.scrollY >50) {
-        setShow(true)
+      // console.log("window.screenY", window.screenY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
       }
-      else {
-        setShow(false)
-      }
-    })
+    });
     return () => {
-      window.removeEventListener("scroll", () => {})
-    }
-  }, [])
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
 
+  useEffect(() => {
+    if (srearchValue) {
+      navigate(`/search?q=${srearchValue}`);
+    }
+  }, [srearchValue, navigate]);
+
+  const handleChange = (e) => {
+    setSrearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
   return (
     <nav className={`nav ${show && "nav__black"}`}>
-      <img 
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" 
-      alt="Netflix logo"
-      className='nav__logo'
-      onClick={() => window.location.reload()}
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+        alt="Netflix logo"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
       />
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Marc_Randolph_by_Gage_Skidmore.jpg/1920px-Marc_Randolph_by_Gage_Skidmore.jpg" 
-      alt="" 
-      className='nav__avatar'
+
+      <div className="nav__inputContainer">
+        <input
+          ref={inputRef}
+          value={srearchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="영화를 검색해주세요"
+        />
+      </div>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Marc_Randolph_by_Gage_Skidmore.jpg/1920px-Marc_Randolph_by_Gage_Skidmore.jpg"
+        alt=""
+        className="nav__avatar"
       />
     </nav>
-  )
+  );
 }
