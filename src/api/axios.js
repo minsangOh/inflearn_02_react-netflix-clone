92595a341483936f0526d0ed9b8d550fe01dd2e6
minsangOh <flos9537@gmail.com>
2023-12-18 16:20:34 +0900

    .env 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params:{
-    api_key:"02f1a9f9fbbe9d262d63c36449dec87e",
+    api_key: process.env.REACT_APP_API_KEY,
     language:"ko-KR",
   }
 })
